605803357c7707dc6b3f079e540525c6585d150f
gurjot74 <gagan.singh@tothenew.com>
2023-03-13 07:32:45 +0530

    changes to selected channel style

--- src/components/Channel.js
+++ src/components/Channel.js
@@ -1,19 +1,32 @@
 import {View, Text, TouchableOpacity} from 'react-native';
 import React from 'react';
 import {StyleSheet} from 'react-native';
-import {useDispatch} from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
 import {selectChannel} from '../Services/Channels/action';
 
 export default function Channel({item}) {
+  const currentChannel = useSelector(
+    state => state.channelReducer.currentChannel,
+  );
   const dispatch = useDispatch();
 
   const handleSelectedChannel = id => {
     dispatch(selectChannel(id));
   };
   return (
-    <View style={styles.channelCard}>
+    <View
+      style={
+        item.meta.id === currentChannel.meta.id
+          ? [styles.channelCard, styles.selected]
+          : styles.channelCard
+      }>
       <TouchableOpacity onPress={() => handleSelectedChannel(item.meta.id)}>
-        <Text style={{fontSize: 20, textAlign: 'center'}}>
+        <Text
+          style={
+            item.meta.id === currentChannel.meta.id
+              ? [styles.cardText, styles.cardSelected]
+              : styles.cardText
+          }>
           {item.meta.title}
         </Text>
       </TouchableOpacity>
@@ -28,5 +41,12 @@ const styles = StyleSheet.create({
     elevation: 5,
     padding: 20,
     margin: 5,
+  },
+  selected: {
+    backgroundColor: 'blue',
+  },
+  cardText: {fontSize: 20, textAlign: 'center'},
+  cardSelected: {
+    color: 'white',
   },
 });
